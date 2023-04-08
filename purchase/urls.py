5fcad8af67432059dd39106fb1c186c83456aae7
mr2space <princegoswami.space@gmail.com>
2023-04-08 12:38:31 +0530

    created and seperated the url for course and code purchase

--- purchase/urls.py
+++ purchase/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("<int:courseId>", views.index, name="purchase"),
-    path("upi/<int:courseId>", views.upiPayment, name="upi"),
-    path("success/",views.payment_success),
-    path("failed/", views.payment_failed),
-    path("webhook/", views.stripeWebhook)
+    path("course/<int:courseId>", views.index, name="purchase"),
+    path("course/upi/<int:courseId>", views.upiPayment, name="upi"),
+    path("code/<int:id>", views.codePurchase, name="purchase"),
+    path("code/upi/<int:id>", views.fileUpiPayment, name="upi file"),
+    path("course/success/<int:purchase_id>/<int:id>",views.course_payment_success),
+    path("course/failed/<int:id>", views.course_payment_failed),
+    path("code/success/<int:purchase_id>/<int:id>",views.code_payment_success),
+    path("code/failed/<int:id>", views.code_payment_failed),
+    path("webhook/", views.stripeWebhook),
 ]
