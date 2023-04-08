2ac26dc126e3dce56a286af8254478c5a1ec68a7
mr2space <princegoswami.space@gmail.com>
2023-04-08 12:39:17 +0530

    added single file url

--- file/urls.py
+++ file/urls.py
@@ -3,6 +3,7 @@ from . import views
 urlpatterns = [
     path("", views.showFiles, name="files"),
     path("new", views.uploadFiles, name="file_upload"),
+    path("code/<int:id>",views.index,name="code img"),
     path("update/<int:id>",views.updateFiles,name="update_files")
 
 ]
