6861a0184cbf2fc9a2c67a334e22fd6c1725be0d
mr2space <princegoswami.space@gmail.com>
2023-04-09 18:40:59 +0530

    TEMP BANDAGE

--- courses/form.py
+++ courses/form.py
@@ -1,8 +1,13 @@
+from bootstrap_datepicker_plus.widgets import DateTimePickerInput
 from django.forms import ModelForm
 from .models import Course
+from django import forms
 
 
 class CourseForm(ModelForm):
     class Meta:
         model = Course
         fields = ['title','title_img', 'short_des', 'description', 'price', 'course_time']
+        widgets = {
+            "course_time": DateTimePickerInput(),
+        }
