28c7a859d9715d3edbeb9cb9e2f193d4de6409a4
mr2space <princegoswami.space@gmail.com>
2023-04-09 11:52:08 +0530

    close msg bug fixed

--- static/base/navbar.js
+++ static/base/navbar.js
@@ -2,6 +2,13 @@
 let menu = document.getElementById("menu");
 let close = document.getElementById("close-sidebar");
 let sidebar = document.getElementById("sidebar");
+
+
+
+function closemsg(){
+document.getElementById("msg-box").classList.add("deactive");
+}
+
 function closeAction(e){
     sidebar.classList.remove("active");
 }
@@ -14,8 +21,3 @@ close.addEventListener("click",closeAction);
 menu.addEventListener("click",openAction);
 
 
-let close_btn = document.querySelector("#msg-box .close-btn");
-
-close_btn.addEventListener("click",()=>{
-    document.getElementById("msg-box").classList.add("deactive");
-})
