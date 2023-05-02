444352fd0a6c10a5d2259470127bdc6d5726c23f
mr2space <princegoswami.space@gmail.com>
2023-05-03 00:37:49 +0530

    settings

--- gadgetsmaker/settings.py
+++ gadgetsmaker/settings.py
@@ -145,10 +145,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles/')
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
+    BASE_DIR / "staticfiles"
 ]
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
