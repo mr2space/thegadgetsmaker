b7e11a2d6a54ef1b66275dcd56549c0fb0015a55
mr2space <princegoswami.space@gmail.com>
2023-05-03 18:38:16 +0530

    settings

--- gadgetsmaker/settings.py
+++ gadgetsmaker/settings.py
@@ -149,7 +149,6 @@ STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles/')
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-    BASE_DIR / "staticfiles"
 ]
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
