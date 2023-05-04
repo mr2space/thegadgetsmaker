5cd631f612caa500b7d1db5571a77080e29ed4df
mr2space <princegoswami.space@gmail.com>
2023-05-04 18:50:24 +0530

    added database

--- purchase/admin.py
+++ purchase/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import PaymentInfo, FailedPayment
+from .models import PaymentInfo, FailedPayment, FilePaymentInfo, FileFailedPayment
 
 
 admin.site.register(PaymentInfo)
 admin.site.register(FailedPayment)
+
+admin.site.register(FilePaymentInfo)
+admin.site.register(FileFailedPayment)
