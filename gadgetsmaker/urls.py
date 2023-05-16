bdc1d852d2d83aa08b7250a5a1c6135e06e74222
mr2space <princegoswami.space@gmail.com>
2023-05-16 21:33:01 +0530

    debug name change

--- gadgetsmaker/urls.py
+++ gadgetsmaker/urls.py
@@ -31,3 +31,7 @@ urlpatterns = [
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
+handler404 = "home.views.page_not_found_view"
